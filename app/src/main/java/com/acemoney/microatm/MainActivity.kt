0049ab64ff224969d045278aef79fe2c12d5c90c
Ashin Aceware <ashindaschandroth97@gmail.com>
2024-05-08 14:20:43 +0530

    first commit

--- app/src/main/java/com/acemoney/microatm/MainActivity.kt
+++ app/src/main/java/com/acemoney/microatm/MainActivity.kt
@@ -1,35 +1,14 @@
-package com.acemoney.microatm;
+package com.acemoney.microatm
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
-import android.app.Activity;
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.util.Log;
-
-import com.acemoney.matm.utils.AceMatm;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-
-public class MainActivity extends AppCompatActivity implements AceMatm.ResponseListener {
-    private AceMatm aceMatm;
-    private String Agent_email;
-    private Activity activity=this;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Agent_email="sruthys240@gmail.com";
-        AceMatm aceMatm= AceMatm.getInstance(activity, Agent_email);
-        aceMatm.setUI(true);
-        aceMatm.setResponseListener(this);
-    }
-
-
-    @Override
-    public void onResult(JsonObject jsonData) {
-        Log.d("dataFROM",new Gson().toJson(jsonData));
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
